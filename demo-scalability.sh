c007b36c624939a97b9be73869ac4680a94eb7bb
kang-xh <kang_xh@hotmail.com>
2020-01-02 15:05:32 +0800

    scale

---
+++ demo-scalability.sh
@@ -0,0 +1,40 @@
+################ Step 1: Get AKS configuration
+
+# get cluster info
+kubectl cluster-info
+kubectl get node -o wide
+kubectl get pod --all-namespaces -o wide
+
+# get voting PoD detials
+kubectl get pod --namespace voting -o wide
+
+# scale voting web front PoD number to 2
+kubectl scale --replicas=2 deployment azure-vote-front --namespace voting
+
+
+################ Step 2: start JMeter for stress test
+Scale JMeter thread to 10,  response around 60ms, Error rate 0
+Scale JMeter thread to 100, response around 120ms, Error rate 0
+Scale JMeter thread to 500, response around 180ms, Error rate 50%
+
+################ Step 3: redo stress test with scale pod
+keep Scale JMeter thread to 500, start to scale pod
+
+kubectl scale --replicas=15 deployment azure-vote-front --namespace voting
+watch kubectl get pod --namespace voting -o wide
+
+Some PoD could be Pending due to Insufficient CPU
+
+JMeter can be stopped as we have demo the pod scale. next is to scale agent pool. 
+
+################ Step 4: Scale node pool to 4 so that all Pods can be scheduled. 
+# scale AKS worknode.
+az account list --output table | grep True
+az aks list --output table
+az aks scale --resource-group az-rg-kangxh-aks --name kangxhakssea --node-count 4 --nodepool-name agentpool
+
+################ Step 5: Scale in 
+kubectl scale --replicas=2 deployment azure-vote-front --namespace voting
+az aks scale --resource-group az-rg-kangxh-aks --name kangxhakssea --node-count 2 --nodepool-name agentpool
+
+
