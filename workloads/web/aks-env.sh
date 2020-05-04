027a07576de101b7739a7fde82e28f2023439279
kang-xh <kang_xh@hotmail.com>
2020-05-04 08:50:27 +0000

    504

--- workloads/web/aks-env.sh
+++ workloads/web/aks-env.sh
@@ -3,5 +3,6 @@
 AKS_PERS_STORAGE_ACCOUNT_NAME=kangxhsaseaweb
 STORAGE_KEY=
 
+kubectl create ns web
 kubectl create secret generic azure-secret --from-literal=azurestorageaccountname=$AKS_PERS_STORAGE_ACCOUNT_NAME --from-literal=azurestorageaccountkey=$STORAGE_KEY -n web
 
