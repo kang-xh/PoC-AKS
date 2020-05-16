4181a0b2e3325adfdb8e02c964a3b0bac1d10a68
kang-xh <kang_xh@hotmail.com>
2020-05-16 09:34:32 +0800

    516

--- workloads/web/aks-env.sh
+++ workloads/web/aks-env.sh
@@ -1,7 +1,7 @@
 # setup enviornment to run the web site. static and configurations saved on Azure File Share.
 
 AKS_PERS_STORAGE_ACCOUNT_NAME=kangxhsaseaweb
-STORAGE_KEY=
+STORAGE_KEY=Bh00uv8QSbRXQffdORxVGx5seQT80+t2eWdLRVnREUjtNCjxVoVoBvNFBPQwlguP33k1VJSx7XxpC62vSLJokw==
 
-kubectl create secret generic azure-secret --from-literal=azurestorageaccountname=$AKS_PERS_STORAGE_ACCOUNT_NAME --from-literal=azurestorageaccountkey=$STORAGE_KEY -n web
+kubectl create secret generic kangxhsaseaweb-secret --from-literal=azurestorageaccountname=$AKS_PERS_STORAGE_ACCOUNT_NAME --from-literal=azurestorageaccountkey=$STORAGE_KEY -n web
 
