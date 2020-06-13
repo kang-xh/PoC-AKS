dca8838d257c817315310a22b27727f8d421ed80
kang-xh <kang_xh@hotmail.com>
2020-06-13 16:29:48 +0800

    613

--- workloads/sql/deploy.sh
+++ workloads/sql/deploy.sh
@@ -1,7 +1,18 @@
 # follow the method in https://docs.microsoft.com/en-us/sql/linux/tutorial-sql-server-containers-kubernetes?view=sql-server-ver15
 cd ~/github/poc-aks/workloads/sql
-kubectl create ns db
-kubectl create secret generic mssql --from-literal=SA_PASSWORD="abcdabcdabcd" -n db
+kubectl create secret generic mssql --from-literal=SA_PASSWORD="defaultdbpassword"
 
 kubectl apply -f pvc.yaml
 kubectl apply -f deploy.yaml
+
+# change disk access right for sql 2019 to fix the following error. 
+
+    SQL Server 2019 will run as non-root by default.
+    This container is running as user mssql.
+    Your master database file is owned by root.
+    To learn more visit https://go.microsoft.com/fwlink/?linkid=2099216.
+    sqlservr: Unable to open /var/opt/mssql/.system/instance_id: Permission denied (13)
+    /opt/mssql/bin/sqlservr: Unable to open /var/opt/mssql/.system//instance_id: Permission denied (13)
+
+connect to 
+
